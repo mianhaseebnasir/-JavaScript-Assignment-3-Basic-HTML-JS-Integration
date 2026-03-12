800fc2c026598dbc5e963664f15966598bfddd61
Mian Haseeb Nasir <mianhaseebnasir1@gmail.com>
2026-03-12 11:53:23 +0500

    Add files via upload

---
+++ Assignment-3 (Javascript Datatypes)/script.js
@@ -0,0 +1,26 @@
+a = prompt( "Enter Your Name");
+b = prompt("Enter Your Father Name");
+alert(a + b);
+console.log(a+b);
+document.writeln(a+b);
+
+// Addition Method a Number
+c = prompt("Enter a 1st number");
+d = prompt("Enter a 2nd number");
+alert(c + d);
+console.log(c + d);
+document.writeln(c + d);
+
+// Subtraction Method a Number
+c = prompt("Enter a 1st number");
+d = prompt("Enter a 2nd number");
+alert(c - d);
+console.log(c - d);
+document.writeln(c - d);
+
+// Multiplication Method a Number
+c = prompt("Enter a 1st number");
+d = prompt("Enter a 2nd number");
+alert(c * d);
+console.log(c * d);
+document.writeln(c * d);
